594a68d12572d1cbc14eef5dd486db79ade7ba47
eneasmarques <eneasmarques@outlook.com.br>
2019-03-07 07:13:59 -0300

    feat: criada classe Movie

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -14,9 +14,11 @@ main_page_head = '''
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="css/styles.css">
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 '''
 
